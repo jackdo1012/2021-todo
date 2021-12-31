030132ed62437d85bdcf1bc2d6e3b30100142c9f
Jack Do <61863362+jackdo1012@users.noreply.github.com>
2021-12-31 22:28:12 +0700

    Set theme jekyll-theme-cayman

--- script.js
+++ script.js
@@ -1,49 +1,52 @@
-document.getElementById("todo1").onclick = function () {
-    const textTodo = document.getElementById("TodoText1")
+const car = document.querySelector("#todo1")
+const money = document.querySelector("#todo2")
+const phone = document.querySelector("#todo3")
+const fat = document.querySelector("#todo4")
+const live = document.querySelector("#todo5")
+car.onclick = () => {
+    const text = document.querySelector("#car")
 
-    if (this.checked) {
-        textTodo.innerHTML = "Mua ô <del>tô</del>"
+    if (car.checked) {
+        text.innerHTML = "Mua ô <del>tô</del>"
     } else {
-        textTodo.innerHTML = "Mua ô tô"
+        text.innerHTML = "Mua ô tô"
     }
 }
 
-document.getElementById("todo2").onclick = function () {
-    const textTodo = document.getElementById("TodoText2")
+money.onclick = () => {
+    const text = document.querySelector("#money")
 
-    if (this.checked) {
-        textTodo.innerHTML = "Tiết kiệm 200.000<del>.000</del>"
+    if (money.checked) {
+        text.innerHTML = "Tiết kiệm 200.000<del>.000</del>"
     } else {
-        textTodo.innerHTML = "Tiết kiệm 200.000.000"
+        text.innerHTML = "Tiết kiệm 200.000.000"
     }
 }
 
-document.getElementById("todo3").onclick = function () {
-    const textTodo = document.getElementById("TodoText3")
+phone.onclick = () => {
+    const text = document.querySelector("#phone")
 
-    if (this.checked) {
-        textTodo.innerHTML = "Thay ốp điện thoại mới"
+    if (phone.checked) {
+        text.innerHTML = "Thay ốp điện thoại mới"
     } else {
-        textTodo.innerHTML = "Thay điện thoại mới"
+        text.innerHTML = "Thay điện thoại mới"
     }
 }
+fat.onclick = () => {
+    const text = document.querySelector("#fat")
 
-document.getElementById("todo4").onclick = function () {
-    const textTodo = document.getElementById("TodoText4")
-
-    if (this.checked) {
-        textTodo.innerHTML = "<del>Giảm</del> béo thành công 5kg"
+    if (fat.checked) {
+        text.innerHTML = "<del>Giảm</del> béo thành công 5kg"
     } else {
-        textTodo.innerHTML = "Giảm béo thành công 5kg"
+        text.innerHTML = "Giảm béo thành công 5kg"
     }
 }
+live.onclick = () => {
+    const text = document.querySelector("#live")
 
-document.getElementById("todo5").onclick = function () {
-    const textTodo = document.getElementById("TodoText5")
-
-    if (this.checked) {
-        textTodo.innerHTML = "Sống <del>cuộc sống an nhàn</del>"
+    if (live.checked) {
+        text.innerHTML = "Sống <del>cuộc sống an nhàn</del>"
     } else {
-        textTodo.innerHTML = "Sống cuộc sống an nhàn"
+        text.innerHTML = "Sống cuộc sống an nhàn"
     }
 }
